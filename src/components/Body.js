3957341912ad73d6b28aac1c5e1d3f4ae698ccf1
Yulia <yuliakorolova@MacBook-Air-Yulia.local>
2025-12-02 00:02:35 +0200

    frontend developer added

--- src/components/Body.js
+++ src/components/Body.js
@@ -16,6 +16,7 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <Card name="Vladislav Namofilov" position="Team Lead" photo="/vladislavNamofilov.png" />
+      <Card name="Yulia Korolova" position="Frontend developer" photo="/yuliaKorolova.png" />
     </main>
   );
 };
